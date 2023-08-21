5250283443cd5a711810287131412709ea549de2
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2023-08-20 21:31:25 -0300

    [upload-service] prisma logs

--- upload-service/src/database.ts
+++ upload-service/src/database.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
 
 export default {
   prisma,
