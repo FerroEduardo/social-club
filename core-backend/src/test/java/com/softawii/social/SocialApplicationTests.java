337b99e2a61960aa2c5920655c640c4e3d232b89
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2023-09-01 13:57:07 -0300

    [pipeline] Build core

--- core-backend/src/test/java/com/softawii/social/SocialApplicationTests.java
+++ core-backend/src/test/java/com/softawii/social/SocialApplicationTests.java
@@ -1,13 +1,7 @@
 package com.softawii.social;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SocialApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
